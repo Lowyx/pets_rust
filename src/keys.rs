e0a7c4150c8a872a647cb7168209611c0113225d
Annika Gabrian <e12404277@student.tuwien.ac.at>
2025-01-10 15:12:49 +0100

    Exercise 1 generate key function

--- src/keys.rs
+++ src/keys.rs
@@ -16,7 +16,14 @@ pub struct KeyPair {
 
 impl KeyPair {
     /// Generate a Schnorr signature key pair
-    pub fn generate() -> KeyPair {}
+    pub fn generate() -> KeyPair {
+        let private_key = Scalar::random(&mut OsRng);
+        let public_key = private_key * RISTRETTO_BASEPOINT_POINT;
+        KeyPair {
+            private_key,
+            public_key,
+        }
+    }
 }
 
 // Unit tests for keys module
